b7d49f8900538681ff59d440af8c74fb04b302c0
Your Name <shahedali2001>
2025-07-11 20:58:09 +0530

    add

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources-${terraform.workspace}"
-    location = "Central India"
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network-${terraform.workspace}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
 resource "azurerm_virtual_network" "example" {
